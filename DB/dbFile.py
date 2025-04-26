f7fbc97dd9d58e50f149178d8d5e1aef8a2a6ad1
TheiCloud <89600381010ln@gmail.com>
2025-04-26 16:19:10 +0300

    db

--- DB/dbFile.py
+++ DB/dbFile.py
@@ -3,9 +3,10 @@ import sqlite3
 
 with sqlite3.connect("data.db") as conn:
     cursor = conn.cursor()
+    # создание таблицы
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS books (
-        id INTEGER PRIMARY_KEY AUTOINCREMENT,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
         author TEXT NOT NULL,
         year_published INTEGER,
@@ -23,18 +24,71 @@ with sqlite3.connect("data.db") as conn:
     result = cursor.fetchone()
 
     if result:
-        insert = '''
-        INSERT INTO books (title, author, year_published, genre)
-        VALUES (?, ?, ?, ?);
+        # insert = '''
+        # INSERT INTO books (title, author, year_published, genre)
+        # VALUES (?, ?, ?, ?);
+        # '''
+        # bookdata = [
+        #     ( "The Great Gatsby", "F. Scott Fitzgerald", 1984, "Fiction" ),
+        #     ( "1984", "George Orwell", 1949, "Dystopian" ),
+        #     ( "To Kill a Mockingbird", "Harper Lee", 1960, "Classic" )
+        #]
+        #cursor.executemany(insert, bookdata)
+        #conn.commit()
+
+        print("задание 3")
+        request = '''
+        SELECT title 
+        FROM books
         '''
-        bookdata = [
-            ( "The Great Gatsby", "F. Scott Fitzgerald", 1984, "Fiction" ),
-            ( "1984", "George Orwell", 1949, "Dystopian" ),
-            ( "To Kill a Mockingbird", "Harper Lee", 1960, "Classic" )
-        ]
-        cursor.executemany(insert, bookdata)
-        conn.commit()
-        
+        cursor.execute(request)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        print("\n")
+
+
+        print("задание 4")
+        request = '''
+        SELECT title 
+        FROM books
+        WHERE year_published > 1950
+        '''
+        cursor.execute(request)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        print("\n")
+
+
+        print("задание 5")
+        request = '''
+                SELECT title 
+                FROM books
+                WHERE title LIKE 'T%';
+                '''
+        cursor.execute(request)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        print("\n")
+
+
+        print("задание 6")
+        request = '''
+        SELECT title FROM books ORDER BY year_published ASC;
+        '''
+        cursor.execute(request)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        print("\n")
+
+
+
+
+
+
     else:
         raise TableNotFound("Table does not exist")
 
